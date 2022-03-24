890a82c4e06ed9abc51a8760ace46564e0316ee5
l-malix <youssef.elmali34@gmail.com>
2022-03-24 11:48:07 +0100

    adding Login and news layouts

--- app/src/main/java/com/example/devmo_agri_ai/CreerCompteActivity.java
+++ app/src/main/java/com/example/devmo_agri_ai/CreerCompteActivity.java
@@ -1,7 +1,10 @@
 package com.example.devmo_agri_ai;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class CreerCompteActivity extends AppCompatActivity {
@@ -10,6 +13,11 @@ public class CreerCompteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_creer_compte);
+    }
+
+    public void lunchLogin(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 
     public void launchAceuilPage(View view) {
